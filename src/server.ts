a30143c1ad1c03f441229a446fea761f0c56ddd6
Viviane Silva <viviane.silva1810@gmail.com>
2024-08-06 17:20:40 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+//GET, POST, PUT, PATCH , DELETE
+// http://localhost:3333/hello
+
+app.get('/hello', () => {
+    return 'hELLO World'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP server running!')
+})
