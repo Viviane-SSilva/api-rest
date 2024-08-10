9e5bdf8459e53744a6bc318fa9536c96b2328aef
Viviane Silva <viviane.silva1810@gmail.com>
2024-08-10 17:29:46 -0300

    Adicionando cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,7 @@
 import { FastifyInstance } from "fastify/types/instance"
 import { knex } from "../database"
 import { z } from "zod"
+import { randomUUID } from "crypto"
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
@@ -44,11 +45,23 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
         const {title, amount, type} = createTransactionBodySchema.parse(request.body)
 
+        let sessionId = request.cookies.sessionId
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+
+            reply.cookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7, // 7 days
+            })
+        }
+
         await knex('transactions')
         .insert({
             id: crypto.randomUUID(),
             title,
             amount: type === "credit" ? amount : amount * -1,
+            session_id: sessionId
 
         })
       
